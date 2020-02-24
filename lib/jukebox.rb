1c8954e261982906dcaa36f43a711652cac3471f
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-24 19:09:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,47 @@
-# Add your code here
+
+# def say_hello(name)
+#   "Hi #{name}!"
+# end 
+
+# puts "Enter your name:"
+# user_name = gets.strip
+
+# puts say_hello(user_name)
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end 
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip 
+    if input.to_i >= 1 && input.to_i <= songs.length 
+        puts "Playing #{songs[input.to_i - 1]}"
+      elsif songs.include?(input)
+        puts "playing #{songs.find"
+        
+      else 
+        puts "Invalid input, please try again"
+      end 
+  end 
+end
+
+
+# def list(playlist)
+#   playlist.map {|song, index| "#{index + 1}. #{song}"}
+# end 
