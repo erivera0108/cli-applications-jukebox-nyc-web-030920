d2d56c8c326083f19c4b97c45267f02e869cd27d
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-24 19:44:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -51,7 +51,9 @@ end
 
 def run(songs) 
   if true 
-  puts "Please enter a command:"
-  input = gets.strip
-end 
+    puts "Please enter a command:"
+    input = gets.strip
+    case input
+    
+  end 
 end 
