9e2a42b5e063cfd2c474c151bacb3c3ff775d8b4
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-24 19:12:35 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -41,7 +41,12 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  input = gets.strip 
+  input = gets.strip
+  if input.to_i >= 1 && input.to_i <= songs.length 
+#         puts "Playing #{songs[input.to_i - 1]}"
+#       elsif
+  songs.each do |song|
+    
 end 
 
 
